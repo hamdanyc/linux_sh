9c4e43dd51e334d6e39fde794e59dec3c1673be9
hamdan <hamdan.hy@gmail.com>
2022-10-01 12:14:22 +0800

    Initial

---
+++ rs_backup.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/sh
+
+rsync -a -v -t --progress \
+/home/abi/media \
+/home/abi/qms \
+/home/abi/classify \
+/home/abi/segi \
+/home/abi/tender \
+# /home/abi/jrnl \
+# abi@192.168.1.116:/home/abi/backup/rs/
+rstudio@abi-linode.name.my:/home/rstudio/
