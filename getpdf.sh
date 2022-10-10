490ec70e194c3f79ec8ad30f6a1d822c287210ae
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2022-10-10 15:17:13 +0800

    Add files via upload

--- getpdf.sh
+++ getpdf.sh
@@ -1,1 +1,3 @@
 cat aset_pertahanan.txt | xargs wget -c
+for i in `ls ~/Documents/aset_pertahanan/pdf/*.pdf`; do pdftotext -layout -nodiag "$i"; done
+awk '/aset|pertahanan/ gsub("aset.pertahanan","*** aset pertahanan ***") {print $0}' *.txt > aset_default
